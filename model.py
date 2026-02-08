527899d628cb690ac18dcf3059727cd88afb8344
Ayush Srivastava <ayush12m2school@gmail.com>
2026-02-08 22:37:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,50 +8,39 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-# ======================================
-# Step 1: Load & Set Features and Target
-# ======================================
 
-# Load your CSV
-#df = pd.read_csv("USGS_processed_1.csv")
+
+
 df = pd.read_csv("USGS_processed_2.csv")
-#print(df.head())
 
-# Features and targets
 X = df[['Month', 'Latitude', 'Longitude']]
 y_mag = df['Magnitude']
 
-# ===========================
-# Step 2: Train Models
-# ===========================
 
-# Magnitude prediction model
+
+
 X_train1, X_test1, y_train1, y_test1 = train_test_split(X, y_mag, test_size=0.2, random_state=42)
 mag_model = RandomForestRegressor(n_estimators=100, random_state=42)
 mag_model.fit(X_train1, y_train1)
 y_mag_pred = mag_model.predict(X_test1)
 
-# ===========================
-# Step 3: Evaluate Models
-# ===========================
+
 
 print("\nModel Evaluation:")
 
-# Mean Absolute Error (MAE)
+
 mae = mean_absolute_error(y_test1, y_mag_pred)
 print(f"Earthquake Magnitude MAE: {mae:.3f}")
 
-# Mean Squared Error (MSE)
+
 mse = mean_squared_error(y_test1, y_mag_pred)
 print(f"Earthquake Magnitude MSE: {mse:.3f}")
 
-# Root Mean Squared Error (RMSE)
+
 rmse = np.sqrt(mse)
 print(f"Earthquake Magnitude RMSE: {rmse:.3f}")
 
-# ===========================
-# Step 4: Save Models (Optional)
-# ===========================
+
 joblib.dump(mag_model, "quake_mag_model.pkl")
 
 '''
@@ -69,4 +58,5 @@ Earthquake Magnitude MAE: 0.320
 Earthquake Magnitude MSE: 0.198
 Earthquake Magnitude RMSE: 0.445
 '''
+
 
